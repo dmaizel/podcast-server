4290b8e854e05b88fe03841f5645b0834d5e64cd
dmaizel <dmaizel@redhat.com>
2020-08-14 09:59:29 +0300

    a

--- go.mod
+++ go.mod
@@ -1,5 +1,7 @@
 module podcast
 
+go 1.15
+
 require (
 	github.com/99designs/gqlgen v0.11.3
 	github.com/vektah/gqlparser/v2 v2.0.1
--- main.go
+++ main.go
@@ -2,37 +2,28 @@ package main
 
 import (
 	"log"
-	"podcast/feeds"
-	"podcast/itunes"
+	"net/http"
+	"os"
+	"podcast/graph"
+	"podcast/graph/generated"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 )
 
+const defaultPort = "8081"
+
 func main() {
-	ias := itunes.NewItunesAPIServices()
-
-	res, err := ias.Search("Full Stack Radio")
-	if err != nil {
-		log.Fatalf("error while searching: %v", err)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
 	}
 
-	for _, item := range res.Results {
-		log.Println("---------------------")
-		log.Printf("Atrist: %s", item.ArtistName)
-		log.Printf("Podcast Name: %s", item.TrackName)
-		log.Printf("Feed url: %s", item.FeedURL)
-		log.Println("---------------------")
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
-		feed, err := feeds.GetFeed(item.FeedURL)
-		if err != nil {
-			log.Fatalf("error while getting feed: %v", err)
-		}
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
 
-		for _, pod := range feed.Channel.Item {
-			log.Println("---------------------")
-			log.Printf("Title: %s", pod.Title)
-			log.Printf("Duration: %s", pod.Duration)
-			log.Printf("Description: %s", pod.Description)
-			log.Printf("URL: %s", pod.Enclosure.URL)
-			log.Println("---------------------")
-		}
-	}
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
